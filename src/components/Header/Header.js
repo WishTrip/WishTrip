78edd89e863bc27b5986caaf4880abf1a5ca453b
Jarid11 <Marinosj1995@gmail.com>
2018-05-22 09:58:00 -0500

    added logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./Header.css"
+import "font-awesome/css/font-awesome.min.css";
 
 import { Link } from "react-router-dom";
 
@@ -7,12 +8,20 @@ class Header extends Component {
 
     render() {
         return (
-            <div className="links-container">
-                <Link to="/" className="header-links">LandingPage</Link>
-                <Link to="/login" className="header-links">Login</Link>
-                <Link to="/plan" className="header-links">Plan</Link>
-                <Link to="/profile" className="header-links">Profile</Link>
-                <Link to="/trips" className="header-links">Trips</Link>
+            <div className="header-container">
+                <div className="logo-wrapper">
+                    <Link to="/" className="header-links logo-container">
+                        <h2>WishTrip</h2>
+                        <i class="fa fa-plane plane-icon"></i>
+                    </Link>
+                </div>
+                <div className="links-container">
+                    <Link to="/" className="header-links">LandingPage</Link>
+                    <Link to="/login" className="header-links">Login</Link>
+                    <Link to="/plan" className="header-links">Plan</Link>
+                    <Link to="/profile" className="header-links">Profile</Link>
+                    <Link to="/trips" className="header-links">Trips</Link>
+                </div>
             </div>
         )
     }
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,9 @@
 import React from "react";
-import "font-awesome";
 
 export default function LandingPage() {
     return (
         <div>
-            <i class="fas fa-plane"></i>
+            LandingPage
         </div>
     )
 }
