4b96ebfbd20b048e7953fc683a6b8f644fd8dea5
Wingcraftian <wingcraftian@gmail.com>
2018-05-22 11:38:56 -0500

    updated

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Profile.css"
+
 
 export default function Profile() {
     return (
-        <div>Profile</div>
+        <div>Profile
+        
+          <div className= "header-container">
+            <input placeholder="Username"  />
+            <input placeholder="First Name"  />
+            <input placeholder="Last Name"  />
+            <input placeholder="Email"  />
+            <div></div>
+            <div></div>
+            </div>
+        </div>
     )
 }
