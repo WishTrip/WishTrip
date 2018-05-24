e8f279769c6fde080422a6ed7d8190264525b4dc
Aaron.Nacua <alnacua@gmail.com>
2018-05-24 12:23:45 -0500

    working login page, seperated firebase code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,29 +2,45 @@ import React, { Component } from "react";
 import "./Home.css";
 import Background from "../Background/Background";
 
-import { connect } from "react-redux"; 
+import { connect } from "react-redux";
 import { toggleHamburgerBtn } from "../../ducks/viewReducer";
+//firebase auth
+import { auth } from "../../firebase";
 
 class Home extends Component {
-
-
-    handleHamburgerMenu = () => {
-        if (!this.props.burgerFlag) {
-            this.props.toggleHamburgerBtn()
-        }
+  state = {
+    currentUser: {}
+  };
+  handleHamburgerMenu = () => {
+    if (!this.props.burgerFlag) {
+      this.props.toggleHamburgerBtn();
     }
+  };
+  componentDidMount() {
+    var user = auth.currentUser;
 
-    render() {
+    if (user) {
+      this.setState({ currentUser: user });
+      console.log(user);
+    }
+  }
+  render() {
     return (
-        <div onClick={() => this.handleHamburgerMenu()}>
-            <Background />
-            <p>Home</p>
-        </div>
-    )}
+      <div onClick={() => this.handleHamburgerMenu()}>
+        <Background />
+
+        {this.state.currentUser && this.state.currentUser.email ? (
+          <h2>Welcome {this.state.currentUser.email}</h2>
+        ) : (
+          <h1> YOU ARE NOT LOGGED IN</h1>
+        )}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    ...state.viewReducer
-})
+  ...state.viewReducer
+});
 
 export default connect(mapStateToProps, { toggleHamburgerBtn })(Home);
