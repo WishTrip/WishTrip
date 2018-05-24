d2ab3177b5702632357b2826f76ed935601de36c
Aaron.Nacua <alnacua@gmail.com>
2018-05-24 12:33:50 -0500

    moved firebase to seperate file, fixed login functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,36 +4,19 @@ import Background from "../Background/Background";
 
 import { connect } from "react-redux";
 import { toggleHamburgerBtn } from "../../ducks/viewReducer";
-//firebase auth
-import { auth } from "../../firebase";
 
 class Home extends Component {
-  state = {
-    currentUser: {}
-  };
   handleHamburgerMenu = () => {
     if (!this.props.burgerFlag) {
       this.props.toggleHamburgerBtn();
     }
   };
-  componentDidMount() {
-    var user = auth.currentUser;
 
-    if (user) {
-      this.setState({ currentUser: user });
-      console.log(user);
-    }
-  }
   render() {
     return (
       <div onClick={() => this.handleHamburgerMenu()}>
         <Background />
-
-        {this.state.currentUser && this.state.currentUser.email ? (
-          <h2>Welcome {this.state.currentUser.email}</h2>
-        ) : (
-          <h1> YOU ARE NOT LOGGED IN</h1>
-        )}
+        <p>Home</p>
       </div>
     );
   }
