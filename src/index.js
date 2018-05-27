cd6ee4c82aae8ecc59e7e3b51f8b2c4cb34778ee
Jacob Anderson <odistiinct@gmail.com>
2018-05-26 22:43:01 -0500

    fixing mistakes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 // import { render } from 'react-dom';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 import { HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme();
 
 ReactDOM.render(
-    <MuiThemeProvider theme={theme}>
-        <Provider store={store}>
-            <Router>
-                <App />
-            </Router>
-        </Provider>
-    </MuiThemeProvider>,
-    document.getElementById('root')
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </MuiThemeProvider>,
+  document.getElementById("root")
 );
