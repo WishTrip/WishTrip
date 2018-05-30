b3c8017dda398fbb3eef5856cd3c04b2938443b8
Jacob Anderson <odistiinct@gmail.com>
2018-05-30 17:05:41 -0500

    dummy comp

--- src/components/DummyAgenda/DummyAgenda.js
+++ src/components/DummyAgenda/DummyAgenda.js
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 import "./DummyAgenda.css";
 import "font-awesome/css/font-awesome.min.css";
 import TimeInput from "material-ui-time-picker";
-import Jin from "./Jin";
+import _ from "lodash.map";
 
 export default class DummyAgenda extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      dots: [],
-      activeDot: 0
+      dots: []
+      // activeDot: this.props.dot && 0
     };
   }
 
   componentDidMount() {
+    console.log(`STATE : => ${this.state.dots.length}`);
     let newDot = [];
     for (let i = 0; i < this.props.length; i++) {
       newDot.push(i);
@@ -23,59 +24,80 @@ export default class DummyAgenda extends Component {
     });
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.length !== this.props.length) {
+      let newDot = [];
+      for (let i = 0; i < this.props.length; i++) {
+        newDot.push(i);
+      }
+      this.setState({
+        dots: newDot
+      });
+    }
+  }
+
   render() {
-    let newDots = this.state.dots.map(dot => {
+    console.log(this.props.mapThis.days[this.props.currentDay][this.props.dot]);
+    console.log(this.props.dotHandler);
+    let newDots = this.state.dots.map((dot, i) => {
       return (
         <i
-          style={{ color: dot === this.state.activeDot ? "blue" : "grey" }}
+          key={i}
+          style={{ color: dot === this.props.dot ? "#999" : "#333" }}
           onClick={() => {
-            this.setState({ activeDot: dot });
+            this.props.toggleDot(dot);
+            // this.setState({ activeDot: dot });
           }}
           className="fa fa-circle"
         />
       );
     });
-
-    let activeComponent = this.state.dots.map(active => {
-      return (
-        <Jin key={active} active={active} activeDot={this.state.activeDot} />
-      );
-    });
+    let agendasMap = (
+      // _(
+      //   this.props.mapThis.days[this.props.currentDay][this.props.dot],
+      //   (e, i) => {
+      //     return (
+      <div className="home-container-wrapper agenda-container-wrapper">
+        {this.props.mapThis.days[this.props.currentDay][this.props.dot].name}
+        {this.props.mapThis.days[this.props.currentDay][this.props.dot].budget}
+        {this.props.mapThis.days[this.props.currentDay][this.props.dot].notes}
+        {/* <div className="home-container"> */}
+        {/* <p className="home-name-input home-inputs">{e.name}</p> */}
+        {/* <div className="home-inputs-container"> */}
+        {/* <div className="home-destination-activity-container"> */}
+        {/* <p className="home-destination-input home-inputs">
+                  {e[this.props.dot].destination}
+                </p> */}
+        {/* <p className="home-activity-input home-inputs">
+                  {e[this.props.dot].activity}
+                </p> */}
+        {/* </div> */}
+        {/* <p className="home-budget-input home-inputs">{e.budget}</p>
+                <p className="home-notes-input  home-inputs">{e.notes}</p> */}
+        {/* <div className="home-time-agenda-container">
+                  <TimeInput
+                    style={{ color: "#fff" }}
+                    className="home-clock"
+                    disabled
+                    mode="12h"
+                    okLabel="submit"
+                    value={time}
+                    onChange={e => this.handleInput("time", e)}
+                  />
+                </div> */}
+        {/* </div> */}
+        {/* </div> */}
+      </div>
+    );
+    {
+      /* );
+      }
+    ); */
+    }
     return (
       <div className="agenda-wrapper">
-        {activeComponent}
         <div className="dot-agendas">{newDots}</div>
-        <p>hello world</p>
-        {/* {mappedDots} */}
-        {/* <h2 className="home-agenda-text">Agenda {agenda}</h2> */}
-        {/* <div className="home-container-wrapper agenda-container-wrapper">
-        <div className="home-container">
-          <p className="home-name-input home-inputs">{saved.name}</p>
-          <div className="home-inputs-container">
-            <div className="home-destination-activity-container">
-              <p className="home-destination-input home-inputs">
-                {saved.destination}
-              </p>
-              <p className="home-activity-input home-inputs">
-                {saved.activity}{" "}
-              </p>
-            </div>
-            <p className="home-budget-input home-inputs">${saved.budget}</p>
-            <p className="home-notes-input  home-inputs">{saved.notes}</p>
-            <div className="home-time-agenda-container">
-              <TimeInput
-                style={{ color: "#fff" }}
-                className="home-clock"
-                disabled
-                mode="12h"
-                okLabel="submit"
-                value={time}
-                onChange={e => this.handleInput("time", e)}
-              />
-            </div>
-          </div>
-        </div>
-      </div> */}
+        {agendasMap}
       </div>
     );
   }
--- src/components/DummyPlan/DummyPlan.js
+++ src/components/DummyPlan/DummyPlan.js
@@ -13,99 +13,174 @@ import TimeInput from "material-ui-time-picker";
 import DummyAgenda from "../DummyAgenda/DummyAgenda";
 
 import { auth } from "../../firebase";
-// user.trips[0].days[0]
-
-const user = {
+//thisuser.trips
+import DummyTripToMap from "./DummyTripMap";
+//thisuser.trips[0].days
+var thisuser = {
   trips: [
-    [
-      {
-        name: "Orlando Family Trip",
-        startDate: new Date(),
-        endDate: new Date(),
-        startLocation: "Dallas, TX",
-        endLocation: "Nashville, TN",
-        cost: "$10,000",
-        days: [
-          [
-            {
-              name: "jarids birthday",
-              budget: "$200",
-              notes: "ice cream, lots of ice cream"
-            },
-            {
-              name: "jarids birthday",
-              budget: "$200",
-              notes: "ice cream, lots of ice cream"
-            },
-            {
-              name: "jarids birthday",
-              budget: "$200",
-              notes: "ice cream, lots of ice cream"
-            }
-          ],
-          [
-            {
-              name: "joes birthday",
-              budget: "$200",
-              notes: "ice cream, lots of ice cream"
-            },
-            {
-              name: "joes birthday",
-              budget: "$200",
-              notes: "ice cream, lots of ice cream"
-            },
-            {
-              name: "joes birthday",
-              budget: "$200",
-              notes: "ice cream, lots of ice cream"
-            }
-          ],
-          [
-            {
-              name: "jacobs birthday",
-              budget: "$200",
-              notes: "ice cream, lots of ice cream"
-            },
-            {
-              name: "jacobs birthday",
-              budget: "$200",
-              notes: "ice cream, lots of ice cream"
-            },
-            {
-              name: "jacobs birthday",
-              budget: "$200",
-              notes: "ice cream, lots of ice cream"
-            }
-          ],
-          [
-            {
-              name: "joes move in day",
-              budget: "$200",
-              notes: "ice cream, lots of ice cream"
-            },
-            {
-              name: "joes move in day",
-              budget: "$200",
-              notes: "ice cream, lots of ice cream"
-            },
-            {
-              name: "joes move in day",
-              budget: "$200",
-              notes: "ice cream, lots of ice cream"
-            }
-          ]
+    {
+      name: "Orlando Family Trip",
+      startDate: new Date(),
+      endDate: new Date(),
+      startLocation: "Dallas, TX",
+      endLocation: "Nashville, TN",
+      cost: "$10,000",
+      days: [
+        [
+          {
+            name: "jarids birthday",
+            budget: "$200",
+            notes: 12
+          },
+          {
+            name: "jarids birthday",
+            budget: "$200",
+            notes: 13
+          },
+
+          {
+            name: "aarons birthday",
+            budget: "$200",
+            notes: "ice cream, lots of ice cream"
+          }
         ],
-        hotels: [
-          "placeholderURL.com",
-          { name: "hotel1", price: "$2,500", image: "image" },
-          { name: "hotel2", price: "$2,500", image: "image" },
-          { name: "hotel3", price: "$2,500", image: "image" },
-          { name: "hotel4", price: "$2,500", image: "image" },
-          { name: "hotel5", price: "$2,500", image: "image" },
-          { name: "hotel6", price: "$2,500", image: "image" }
+        [
+          {
+            name: 1,
+            budget: "$200",
+            notes: 11
+          },
+          {
+            name: 2,
+            budget: "$200",
+            notes: 21
+          },
+          {
+            name: "joes birthday",
+            budget: "$200",
+            notes: 22
+          },
+          {
+            name: "joes birthday",
+            budget: "$200",
+            notes: 23
+          }
+        ],
+        [
+          {
+            name: 3,
+            budget: "$200",
+            notes: 31
+          },
+          {
+            name: "jacobs birthday",
+            budget: "$200",
+            notes: 32
+          },
+          {
+            name: "jacobs birthday",
+            budget: "$200",
+            notes: 33
+          }
+        ],
+        [
+          {
+            name: 4,
+            budget: "$200",
+            notes: 41
+          },
+          {
+            name: "joes move in day",
+            budget: "$200",
+            notes: 42
+          },
+          {
+            name: "joes move in day",
+            budget: "$200",
+            notes: 43
+          }
         ]
-      }
-    ]
+      ],
+      hotels: [
+        "placeholderURL.com",
+        { name: "hotel1", price: "$2,500", image: "image" },
+        { name: "hotel2", price: "$2,500", image: "image" },
+        { name: "hotel3", price: "$2,500", image: "image" },
+        { name: "hotel4", price: "$2,500", image: "image" },
+        { name: "hotel5", price: "$2,500", image: "image" },
+        { name: "hotel6", price: "$2,500", image: "image" }
+      ]
+    },
+    {
+      name: "yolo dog Trip",
+      startDate: new Date(),
+      endDate: new Date(),
+      startLocation: "Dallas, TX",
+      endLocation: "Nashville, TN",
+      cost: "$10,000",
+      days: [
+        [
+          {
+            name: "jarids birthday",
+            budget: "$200",
+            notes: "ice cream, lots of ice cream"
+          },
+          {
+            name: "jarids birthday",
+            budget: "$200",
+            notes: "ice cream, lots of ice cream"
+          },
+          {
+            name: "jarids birthday",
+            budget: "$200",
+            notes: "ice cream, lots of ice cream"
+          }
+        ],
+        [
+          {
+            name: "joes birthday",
+            budget: "$200",
+            notes: "ice cream, lots of ice cream"
+          },
+          {
+            name: "joes birthday",
+            budget: "$200",
+            notes: "ice cream, lots of ice cream"
+          },
+          {
+            name: "joes birthday",
+            budget: "$200",
+            notes: "ice cream, lots of ice cream"
+          }
+        ],
+        [
+          {
+            name: "jacobs birthday",
+            budget: "$200",
+            notes: "ice cream, lots of ice cream"
+          },
+          {
+            name: "jacobs birthday",
+            budget: "$200",
+            notes: "ice cream, lots of ice cream"
+          },
+          {
+            name: "jacobs birthday",
+            budget: "$200",
+            notes: "ice cream, lots of ice cream"
+          }
+        ]
+      ],
+      hotels: [
+        "placeholderURL.com",
+        { name: "hotel1", price: "$2,500", image: "image" },
+        { name: "hotel2", price: "$2,500", image: "image" },
+        { name: "hotel3", price: "$2,500", image: "image" },
+        { name: "hotel4", price: "$2,500", image: "image" },
+        { name: "hotel5", price: "$2,500", image: "image" },
+        { name: "hotel6", price: "$2,500", image: "image" }
+      ]
+    }
   ]
 };
 
@@ -113,280 +188,30 @@ class DummyPlan extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      day: 1,
-      currentDay: 1,
-      newDay: true,
-      agenda: 1,
-      currentAgenda: 1,
-      time: new Date(),
-      agendaNameInput: "",
-      destinationInput: "",
-      activityInput: "",
-      budgetInput: 0,
-      notesInput: "",
-      nextStepsFlag: false,
-      dotHandler: 0
+      currentTrip: 0
     };
-    this.handleInput = this.handleInput.bind(this);
-    this.handleDay = this.handleDay.bind(this);
-    this.handleAgenda = this.handleAgenda.bind(this);
-    this.decrementDay = this.decrementDay.bind(this);
-    this.incrementDay = this.incrementDay.bind(this);
-    this.handleCompleteDay = this.handleCompleteDay.bind(this);
   }
-  toggleDot = val => {
-    console.log(val);
-    this.setState({ dotHandler: val });
+  updateCurrentTrip = val => {
+    this.setState({ currentTrip: val });
   };
-  handleHamburgerMenu = () => {
-    if (!this.props.burgerFlag) {
-      this.props.toggleHamburgerBtn();
-    }
-  };
-
-  handleInput(key, val) {
-    if (key === "budgetInput") {
-      return this.setState({
-        [key]: parseInt(val, 10)
-      });
-    }
-    this.setState({
-      [key]: val
-    });
-  }
-
-  handleDay() {
-    let { day, currentDay } = this.state;
-    this.setState({
-      day: day + 1,
-      currentDay: currentDay + 1,
-      currentAgenda: 1,
-      newDay: true
-    });
-  }
-
-  decrementDay() {
-    let { day, currentDay } = this.state;
-    this.setState({
-      day: day - 1,
-      currentDay: currentDay - 1
-    });
-  }
-
-  incrementDay() {
-    let { day, currentDay } = this.state;
-    this.setState({
-      day: day + 1,
-      currentDay: currentDay + 1
-    });
-  }
-
-  handleAgenda(
-    agendaNameInput,
-    destinationInput,
-    activityInput,
-    budgetInput,
-    notesInput,
-    time
-  ) {
-    let { currentAgenda, newDay, currentDay } = this.state;
-    if (newDay) {
-      this.props.saveAgenda(
-        newDay,
-        currentDay,
-        currentAgenda,
-        agendaNameInput,
-        destinationInput,
-        activityInput,
-        budgetInput,
-        notesInput,
-        time
-      );
-      this.setState({
-        newDay: false
-      });
-    } else {
-      this.props.saveAgenda(
-        newDay,
-        currentDay,
-        currentAgenda,
-        agendaNameInput,
-        destinationInput,
-        activityInput,
-        budgetInput,
-        notesInput,
-        time
-      );
-    }
-
-    this.setState({
-      nextStepsFlag: true,
-      currentAgenda: currentAgenda + 1,
-      agendaNameInput: "",
-      destinationInput: "",
-      activityInput: "",
-      budgetInput: 0,
-      notesInput: ""
-    });
-  }
-
-  handleCompleteDay() {
-    if (auth.currentUser) {
-      console.log(auth.currentUser);
-      this.props.completeTrip(this.props.days);
-      this.props.sendUserInfo(this.props.user);
-    } else {
-      window.location = "/#/login";
-      this.props.completeTrip(this.props.days);
-    }
-  }
 
   render() {
-    const {
-      day,
-      agenda,
-      currentDay,
-      currentAgenda,
-      time,
-      agendaNameInput,
-      destinationInput,
-      activityInput,
-      budgetInput,
-      notesInput,
-      nextStepsFlag
-    } = this.state;
+    console.log(this.state.currentTrip);
     const { days } = this.props;
     let amountOfDays = days.length;
-    // [0].days[0]
-    // let currentAgendas = user.trips.map((e, i) => {
-    //   return <DummyAgenda key={i} length={3} />;
-    // });
+    let mappedTrips = thisuser.trips.map((e, i, a) => {
+      console.log(i, this.state.currentTrip, a[i]);
+      return (
+        <DummyTripToMap
+          currentTrip={this.state.currentTrip === i ? true : false}
+          updateCurrentTrip={this.updateCurrentTrip}
+          trips={thisuser.trips}
+          mapthis={e}
+        />
+      );
+    });
     console.log(this.props);
-    return (
-      <div>
-        <div>
-          <div
-            className="home-wrapper"
-            onClick={() => this.handleHamburgerMenu()}
-          >
-            <Background />
-            <h2 className="home-agenda-text">Trip Name</h2>
-            <div className="home-day-container home-chevron">
-              <i
-                onClick={() => this.decrementDay()}
-                className={day === 1 ? null : "fa fa-chevron-left"}
-              />
-              <h1 className="home-day-text">Day {day}</h1>
-              <i
-                onClick={() => this.incrementDay()}
-                className={days[currentDay - 1] ? "fa fa-chevron-right" : null}
-              />
-            </div>
-
-            <div className="home-container-wrapper">
-              {!this.props.user.trips.length ? (
-                <DummyAgenda
-                  length={6}
-                  toggleDot={this.toggleDot}
-                  dot={this.state.dotHandler}
-                />
-              ) : (
-                <div className="home-container">
-                  <input
-                    className="home-name-input home-inputs"
-                    type="text"
-                    placeholder="Agenda Name"
-                    value={agendaNameInput}
-                    onChange={e =>
-                      this.handleInput("agendaNameInput", e.target.value)
-                    }
-                  />
-                  <div className="home-inputs-container">
-                    <div className="home-destination-activity-container">
-                      <input
-                        className="home-destination-input home-inputs"
-                        type="text"
-                        placeholder="Agenda Destination"
-                        value={destinationInput}
-                        onChange={e =>
-                          this.handleInput("destinationInput", e.target.value)
-                        }
-                      />
-                      <input
-                        className="home-activity-input home-inputs"
-                        type="text"
-                        placeholder="Agenda Activity"
-                        value={activityInput}
-                        onChange={e =>
-                          this.handleInput("activityInput", e.target.value)
-                        }
-                      />
-                    </div>
-                    <div className="budget-container">
-                      <i className="home-dollar-sign">$</i>
-                      <input
-                        className="home-budget-input-position  home-budget-input home-inputs"
-                        type="number"
-                        placeholder="Budget for Day"
-                        value={budgetInput}
-                        onChange={e =>
-                          this.handleInput("budgetInput", e.target.value)
-                        }
-                      />
-                    </div>
-                    <textarea
-                      className="home-notes-input  home-inputs"
-                      type="text"
-                      placeholder="import notes, blah, blah, blah.."
-                      value={notesInput}
-                      onChange={e =>
-                        this.handleInput("notesInput", e.target.value)
-                      }
-                    />
-                    <div className="home-time-agenda-container">
-                      <TimeInput
-                        className="home-clock"
-                        mode="12h"
-                        okLabel="submit"
-                        value={time}
-                        onChange={e => this.handleInput("time", e)}
-                      />
-                      <button
-                        className="home-save-agenda-btn"
-                        onClick={() =>
-                          this.handleAgenda(
-                            agendaNameInput,
-                            destinationInput,
-                            activityInput,
-                            budgetInput,
-                            notesInput,
-                            time
-                          )
-                        }
-                      >
-                        Add Agenda
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div>
-            {nextStepsFlag ? (
-              <div>
-                <button
-                  onClick={() => {
-                    this.handleCompleteDay();
-                  }}
-                >
-                  Complete Trip
-                </button>
-                <button onClick={() => this.handleDay()}>Add New Day</button>
-              </div>
-            ) : null}
-          </div>
-        </div>
-      </div>
-    );
+    return <div className="blahblah">{mappedTrips}</div>;
   }
 }
 
