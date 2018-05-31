a50f0ca7abbfd8489321a216ae5608e73118831c
Aaron Blackshear <theshiftybapple@gmail.com>
2018-05-31 11:10:44 -0500

    Storing current user trips in redux

--- server/controllers/databaseController.js
+++ server/controllers/databaseController.js
@@ -36,22 +36,25 @@ const loginUser = (req, res) => {
 const sendUserInfo = (req, res) => {
   const { user } = req.body;
 console.log(user)
-  // database
-  //   .ref(`users/${user.userinfo.uid}`)
-  //   .child("userinfo")
-  //   .set({ email: user.userinfo.email });
+  database
+    .ref(`users/${user.userinfo.uid}`)
+    .child("userinfo")
+    .set({ email: user.userinfo.email });
 
-  // database
-  //   .ref(`users/${user.userinfo.uid}/trips`)
-  //   .child(`${user.trips[0].tripName}`)
-  //   // .push()
-  //   .set({
-  //     tripBudget: user.trips[0].tripBudget,
-  //     tripLocation: user.trips[0].tripLocation,
-  //     tripName: user.trips[0].tripName,
-  //     tripNotes: user.trips[0].tripNotes,
-  //     days: user.trips[0].days
-  //   });
+  database
+    .ref(`users/${user.userinfo.uid}/trips`)
+    .child(`${user.trips[0].name}`)
+    // .push()
+    .set({
+      name: user.trips[0].name,
+      origin: user.trips[0].origin,
+      destination: user.trips[0].destination,
+      starting: user.trips[0].starting,
+      ending: user.trips[0].ending,
+      budget: user.trips[0].budget,
+      notes: user.trips[0].notes,
+      days: user.trips[0].days
+    });
 
   //----------- CHECK TRIP CODE -------------//
   // database.ref("users").once("value", snap => {
